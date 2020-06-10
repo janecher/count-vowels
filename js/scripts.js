a024d1c90cf09dadb84a4e7a9e9c1bc1bb25ce13
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-10 09:57:37 -0700

    add function to count the vowels
    Co-authored-by: Micheal Hansen <micheal.hansen@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
+//business logic
+const countVowels = function(sentence) {
+  let numberOfVowels = 0;
+  const vowels = ["a", "o", "i", "u", "e", "A", "O", "I", "U", "E"];
+  for(let i = 0; i < sentence.length; i++){
+    if (vowels.includes(sentence.charAt(i))) {
+      numberOfVowels++;
+    };
+  }
+  return numberOfVowels;
+}
 
+//user interface logic
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    const sentence = $("input").val();
+
+  });
 });
